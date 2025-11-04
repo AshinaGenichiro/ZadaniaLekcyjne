070b44e323078d54c8f7f646c25275c7f981f630
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2025-11-04 14:07:01 +0100

    zadanie 8.2 z c#

---
+++ generatorList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata;
+
+namespace generatorList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe zdań (1-20):");
+            string zdania = Console.ReadLine();
+            if (int.TryParse(zdania, out int liczbaZdan))
+            {
+                if (liczbaZdan >= 1 && liczbaZdan <= 20)
+                {
+                    for (int i = 1; i <= liczbaZdan; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine($"Zdanie {i} - priorytet normalny");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Zdanie {i} - priorytet wysoki");
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
